69060efcf17042759180100a82dd2e6d9c2399f4
aspsnd <2546697613@qq.com>
2021-11-26 15:51:49 +0800

    dev

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-require('../dist/index.js').pack();
+const minimist = require("minimist");
+const { pack } = require('../dist');
+
+const args = minimist(process.argv.slice(2));
+
+const targets = args._;
+
+for (const target of targets) {
+  pack()
+}
