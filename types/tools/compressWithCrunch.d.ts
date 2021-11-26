7224823c7142766a400df20a3657d39f545141f1
aspsnd <2546697613@qq.com>
2021-11-26 16:30:52 +0800

    dev

--- types/tools/compressWithCrunch.d.ts
+++
@@ -1,5 +0,0 @@
-import { SinglePackOptions } from '../index';
-/**
- * Compress texture with the S3TC compression format
- */
-export declare const compressWithCrunch: (args: Required<SinglePackOptions<'s3tc'>>) => Promise<any>;
--- types/tools/compressWithPVRTexTool.d.ts
+++
@@ -1,5 +0,0 @@
-import { SinglePackOptions } from '..';
-/**
- * Compress texture with the ASTC, ETC or PVRTC compression format
- */
-export declare const compressWithPVRTexTool: (args: Required<SinglePackOptions<'astc' | 'etc' | 'pvrtc'>>) => Promise<any>;
